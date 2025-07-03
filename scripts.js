8a583d63ddcd0da61d03ee271fc18eb32e615eed
Rodrigo Eduardo <eduardorodrigoalmeida18@gmail.com>
2025-07-03 14:55:38 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+/* 
+VÁRIAVEIS - Um pedacinho de memória do computador
+que eu posso guardar o que eu quiser.
+
+FUNCOES
+É um pedacinho de código QUE, só executa 
+Quando é chamado.
+
+documet = HTML
+querySelector = buscar alguém no HTML
+
+[x] Descobrir quando o botão foi clicado
+[x] Pegar o que foi escrito no Input
+[ ] Enviar para o N8N
+[ ] Receber o que o N8N Respondeu
+[ ] Colocar na Tela o que ele respondeu    
+
+*/
+
+
+function cliqueiNoBotao() {
+    let textoInput = document.querySelector(".input-animacao").value
+
+
+
+
+}
